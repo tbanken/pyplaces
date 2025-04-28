b036d1cd83b863e6ad34d94ecfc9597b4487a328
Ted <tedbanken@gmail.com>
2025-04-28 12:45:53 -0400

    removed unnecessary error type

--- src/pyplaces/_errors.py
+++ src/pyplaces/_errors.py
@@ -3,6 +3,3 @@ class UnsupportedOperatorError(NotImplementedError):
 
 class S3ReadError(FileNotFoundError):
     """Exception for empty results when querying an S3 bucket."""
-
-class PyArrowError(ValueError):
-    """Exception for catching PyArrow errors when invalid parameters are passed."""
